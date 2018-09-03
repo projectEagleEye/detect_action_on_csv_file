85d3fb8aa9aa13fae964b15f5ab54ac04cb7b100
Shi Jie Wei <barneywei@hotmail.com>
2018-09-03 17:17:01 -0400

    Updated: - changed "transposition" default from True to False - corrected docstring

--- csv_reader.py
+++ csv_reader.py
@@ -14,14 +14,14 @@ def get_processed_data(csv_file,
                        sensor_cols,
                        data_type_col,
                        data_type,
-                       transposition=True):
+                       transposition=False):
     """
     main function that returns processed data from csv
     :param csv_file: STRING - file path directory
     :param sensor_cols: INT LIST - integer index of sensor columns in csv file
     :param data_type_col: INT - integer index of "data_type"
     :param data_type: STRING - name of the type of csv data to collect (eg. "Person0/eeg"
-    :param transposition: BOOLEAN - transpose the csv data output 2D numpy array (default=True)
+    :param transposition: BOOLEAN - transpose the csv data output 2D numpy array (default=False)
     :return: NUMPY FLOAT ARRAY - processed data
     """
     # perform error checking on parameters
@@ -79,7 +79,7 @@ def process_data(raw_data,
     :param sensor_cols: INT ARRAY - integer index of sensor columns in csv file
     :param data_type_col: INT - integer index of "data_type"
     :param data_type: STRING - name of the type of csv data to collect (eg. " Person0/eeg"
-    :param data_type: BOOLEAN - transpose the csv data output 2D numpy array
+    :param transposition: BOOLEAN - transpose the csv data output 2D numpy array (default=False)
     :return: NUMPY FLOAT ARRAY - processed data
     """
     # initiate a numpy array
