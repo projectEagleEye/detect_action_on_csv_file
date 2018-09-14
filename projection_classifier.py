9fd4c39fda2d533bcf2d10167cf67a80c7703091
Barney Wei <barneywei@hotmail.com>
2018-09-14 18:42:03 -0400

    Updated: - created and finished get_reference_matrix_magnitude() - completed but haven't bug-checked get_projection_classification() so it probably doesn't work yet - created functions to read and write reference_vectors to text files - use numpy.core.umath_tests for dot product to increase speed - other smaller fixes

--- projection_classifier.py
+++ projection_classifier.py
@@ -7,6 +7,7 @@
 
 # import necessary libraries and files
 import numpy as np
+from numpy.core.umath_tests import inner1d
 import csv_analytics
 
 
@@ -14,7 +15,7 @@ def get_action(ports_stream, use_default_mean):
     """
     function that takes in "ports_stream" parameter to classify a body action based on the data
     stream
-    :param ports_stream: NUMPY ARRAY
+    :param ports_stream: NUMPY 2D ARRAY
     :return: STRING - classified action in a numpy list
     """
 
@@ -41,13 +42,13 @@ def get_signal_3d_tensor(ports_stream,
                          voltage_interval):
     """
     function that output signal vectors for each port found from the "ports_stream_df"
-    :param ports_stream: NUMPY ARRAY
-    :param calibration_mean: NUMPY FLOAT ARRAY
+    :param ports_stream: NUMPY 2D ARRAY
+    :param calibration_mean: NUMPY 1D FLOAT ARRAY
     :param time_interval: INT - the interval the signal holds above or below the voltage-interval on the
     calibration_mean to begin or end recording of the signal vector
     :param voltage_interval: BOOLEAN LIST - the interval above or below the calibration_mean which triggers recording
     of the signal vector when breached (for each signal port)
-    :return: LIST - contains the signal vectors for each port as a list of 2D numpy arrays
+    :return: LIST OF NUMPY 2D ARRAY - contains the signal vectors for each port as a list of 2D numpy arrays
     """
 
     # instantiate variables
@@ -126,18 +127,18 @@ def get_signal_3d_tensor(ports_stream,
 
 
 def get_reference_matrix(ports_stream,
-                          calibration_mean,
-                          time_interval,
-                          voltage_interval):
+                         calibration_mean,
+                         time_interval,
+                         voltage_interval):
     """
     function that output reference vectors for a body action
-    :param ports_stream: NUMPY ARRAY - contain multiple repeated signal vectors of the desired body action
-    :param calibration_mean: NUMPY FLOAT ARRAY
+    :param ports_stream: NUMPY 2D ARRAY - contain multiple repeated signal vectors of the desired body action
+    :param calibration_mean: NUMPY 1D FLOAT ARRAY
     :param time_interval: INT - the interval the signal holds above or below the voltage-interval on the
     calibration_mean to begin or end recording of the signal vector
     :param voltage_interval: INT - the interval above or below the calibration_mean which triggers recording of the signal
     vector when breached
-    :return: NUMPY ARRAY - signal vectors for each port
+    :return: NUMPY 2D ARRAY - signal vectors for each port
     """
     # get the number of rows and columns from the dataset
     num_cols = ports_stream.shape[1]
@@ -151,9 +152,6 @@ def get_reference_matrix(ports_stream,
         if max_rows < signal_matrix.shape[0]:
             max_rows = signal_matrix.shape[0]
 
-    # initiate numpy array to store reference vectors
-    reference_vectors = np.zeros((max_rows, 0))
-
     # initialize temporary 3d tensor to hold all signal matrices after zero padding
     temp_signal_3d_tensor = np.zeros((max_rows, num_cols, 0))
     for signal_matrix in signal_3d_tensor:
@@ -164,22 +162,81 @@ def get_reference_matrix(ports_stream,
         temp_signal_3d_tensor = np.dstack((temp_signal_3d_tensor, signal_matrix))
 
     # get reference vector through finding the mean among vectors of the same column and skipping nan values
-    reference_vectors = np.nanmean(temp_signal_3d_tensor, axis=2)
+    reference_matrix = np.nanmean(temp_signal_3d_tensor, axis=2)
 
-    return reference_vectors
+    return reference_matrix
 
 
-def get_projection_classification(signal_3d_tensor, reference_vectors):
+def get_reference_matrix_magnitude(reference_matrix):
+    """
+    function that returns the magnitude of each column in the reference_matrix as a numpy array
+    :param reference_matrix: NUMPY 2D ARRAY
+    :return: NUMPY 1D ARRAY
+    """
+    reference_matrix_magnitude = np.sqrt(inner1d(reference_matrix.T, reference_matrix.T))
+
+    return reference_matrix_magnitude.reshape(1, reference_matrix.shape[1])
+
+
+def get_projection_classification(signal_3d_tensor, reference_3d_tensor, reference_3d_tensor_magnitude):
     """
     function that take each batch of signal vectors and compare to each batch of reference vectors by projection. The
     projection action with the highest value indicates highest similarity with the reference action
-    :param signal_3d_tensor: NUMPY ARRAY 2D LISTS - shape=(vector_length, num_ports, num_vectors)
-    :param reference_vectors: NUMPY ARRAY 2D LISTS - shape=(vector_length, num_ports, num_actions)
-    :return: INT - index into which action reference vector the scalar projection value was the highest
+    :param signal_3d_tensor: LIST OF NUMPY 2D ARRAY - shape=(vector_length, num_ports, num_vectors)
+    :param reference_3d_tensor: LIST OF NUMPY 2D ARRAY - shape=(vector_length, num_ports, num_actions)
+    :param reference_3d_tensor_magnitude: NUMPY 1D ARRAY - shape(num_actions, num_ports)
+    :return: NUMPY 1D ARRAY - index into which action reference vector the scalar projection value was the highest
     """
     # TODO: get_projection_classification()
+    # instantiate variables
+    num_cols = signal_3d_tensor[0].shape[1]
+    projection_matrix = np.zeros((0, len(reference_3d_tensor)))
 
-    return
+    # iterate through signal_3d_tensor for projection
+    for signal_matrix in signal_3d_tensor:
+        projection_array = []
+        for i in range(len(reference_3d_tensor)):
+            # 0-pad matrix to reach equal number of rows
+            num_padding = abs(signal_matrix.shape[0] - reference_matrix.shape[0])
+            if signal_matrix.shape[0] < reference_3d_tensor[i].shape[0]:
+                temp_signal_matrix = np.pad(signal_matrix, ((0, num_padding), (0, 0)), "constant", constant_values=0)
+            elif signal_matrix.shape[0] > reference_3d_tensor[i].shape[0]:
+                temp_reference_matrix = np.pad(reference_3d_tensor[i], ((0, num_padding), (0, 0)), "constant", constant_values=0)
+
+            # extract the scalar when projecting signal_matrix onto reference_matrix
+            temp_element = inner1d(signal_matrix.T, signal_matrix.T)  # dot product
+            temp_element = np.divide(temp_element, np.square(reference_3d_tensor_magnitude[i]))
+            temp_element = np.sum(temp_element, axis=1)
+            temp_element = np.subtract(temp_element, num_cols)
+            temp_element = np.abs(temp_element)
+            projection_array += temp_element
+        projection_matrix = np.append(projection_matrix, projection_array, axis=0)
+
+    # determine reference_3d_tensor index for classify action
+    classificaton_index = np.argmin(projection_matrix, axis=1)
+    
+    return classificaton_index
+
+
+def save_reference_matrix_to_txt(reference_matrix, action_name, path="/ref_matrix"):
+    """
+    function that saves reference_matrix to a text file
+    :param reference_matrix: NUMPY 2D ARRAY
+    :param action_name: STRING - the body action defined by the reference_matrix
+    :param path: STRING - save path (default="/ref_matrix")
+    :return: BOOLEAN - whether if save is successful
+    """
+    # TODO: save_reference_matrix_to_txt() - use np.savetxt()
+
+
+def read_reference_matrix_from_txt(file_name, path="/ref_matrix"):
+    """
+    function that reads reference_matrix from text file
+    :param file_name: STRING
+    :param path: STRING - read path (default="/ref_matrix")
+    :return: NUMPY ARRAY
+    """
+    # TODO: read_reference_matrix_from_txt() - use np.loadtxt()
 
 
 # CODE TESTING
@@ -201,19 +258,31 @@ if __name__ == "__main__":
                                                  data_type,
                                                  transposition)
     calibration_mean = csv_analytics.get_calibration_mean(ports_stream)
-    time_interval = 22
+    time_interval = 12
     voltage_interval = [80, 20, 25, 80]
 
     # TEST: get_signal_3d_tensor()
+    print("TEST: get_signal_3d_tensor()")
     signal_3d_tensor = get_signal_3d_tensor(ports_stream, calibration_mean, time_interval, voltage_interval)
-    print(signal_3d_tensor)
+    # print(signal_3d_tensor)
     for i in range(len(signal_3d_tensor)):
         print(signal_3d_tensor[i].shape)
     print(len(signal_3d_tensor))
     print("My program took", time.time() - start_time, "seconds to run")
+    print("____________________________")
 
     # TEST: get_reference_matrix()
-    """reference_matrix = get_reference_matrix(ports_stream, calibration_mean, time_interval, voltage_interval)
-    print(reference_matrix)
-    print(reference_matrix.shape)"""
+    print("TEST: get_reference_matrix()")
+    reference_matrix = get_reference_matrix(ports_stream, calibration_mean, time_interval, voltage_interval)
+    # print(reference_matrix)
+    print(reference_matrix.shape)
+    print("____________________________")
 
+    # TEST: get_reference_matrix_magnitude()
+    reference_matrix_magnitude = get_reference_matrix_magnitude(reference_matrix)
+    print(reference_matrix_magnitude)
+
+    # TEST: get_projection_classification()
+    """print("TEST: get_projection_classification()")
+    reference_3d_tensor = [reference_matrix]
+    print("____________________________")"""
