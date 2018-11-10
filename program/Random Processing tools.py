96c7f01e51b001dc2e55af74b04c2e6c49952209
Evan Pan <evan12345pan@hotmail.com>
2018-11-09 19:51:06 -0500

    added processing tools
    Added Fourier transform, sum and difference of left and right

--- program/Random Processing tools.py
+++ program/Random Processing tools.py
@@ -2,7 +2,8 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
-from scipy import signal
+from biosppy.signals.tools import filter_signal as Filter
+from biosppy.signals.tools import normalize as normalize
 
 def get_raw_data(csv_file):
 
@@ -27,19 +28,19 @@ def get_Specifc_data(dataType, storage):
 	for item in storage:
 		if item[1] == dataType:
 			rtv = rtv + [item[2:6]]
-	print(len(rtv[1]))
+	# print(len(rtv[1]))
 	return rtv
-def Decrease_sampling(n_4_matrix, frequency): # This is shitty and does not decrease noise 
+def Decrease_sampling(n_4_matrix, frequency):
 	rtl = []
 	interval = int(256/frequency)
 	for i in range (0, len(n_4_matrix), interval):
 		rtl = rtl + [n_4_matrix[i]]
 	return rtl 
-def normalize(data):
-	avg = sum(data)/len(data)	
-	for i in range (0, len(data)):
-		data[i] = data[i] - avg;
-	return data; # this brings all the datapoints to zero for one list
+# def normalize(data):
+# 	avg = sum(data)/len(data)	
+# 	for i in range (0, len(data)):
+# 		data[i] = data[i] - avg;
+# 	return data;
 def get_deri(x,y,freq):
 	dx = (1/freq)
 	rtv = []
@@ -50,67 +51,51 @@ def get_deri(x,y,freq):
 		rtv = rtv + [deriv_val]
 	print(len(rtv))
 	print(len(y))
-	return rtv # the derivative
+	return rtv
 
-def FFT(data, cut_off = 10):
-	cut_off = (cut_off/256.0)
-	b = 0.08
-	N = int(np.ceil((4 / b)))
-	if not N % 2: N += 1
-	n = np.arange(N) 
-	sinc_func = np.sinc(2 * cut_off * (n - (N - 1) / 2.))
-	window = 0.42 - 0.5 * np.cos(2 * np.pi * n / (N - 1)) + 0.08 * np.cos(4 * np.pi * n / (N - 1))
-	sinc_func = sinc_func * window
-	sinc_func = sinc_func / np.sum(sinc_func)
 
-	s = list(data)
-	new_signal = np.convolve(s, sinc_func)
-	return new_signal
+####################################### get signal ########################################### 
 
-thing = get_raw_data('LookDown30.csv')
-thing = get_Specifc_data(" /muse/notch_filtered_eeg",thing)
+thing = get_raw_data('Blinks1.csv')
+thing = get_Specifc_data(" Person0/notch_filtered_eeg",thing)
 
+# getting the transverse matrix 
 data = [[],[],[],[]]
 for i in range (0, len(thing)):
 	for j in range (0,4):
 		data[j] = data[j] + [round(float(thing[i][j]),3)]
 
-print (thing)
-y = data[0]
-x = list(range (0, len(y)));
-y = normalize(y);
+####################################### Process data ########################################### 
+processed_data = [[],[],[],[]] 
+# normalize the data, making them all ossillate around 0;
+for i in range (0, 4):
+	data[i] = normalize(data[i])[0]
+# find sum and difference of signals to amplify 
+for i in range (0, len(data[0])):
+	processed_data[0] = processed_data[0] + [data[3][i] + data[0][i]] # sum of temple data
+	processed_data[1] = processed_data[1] + [data[2][i] + data[1][i]] # sum of brow data
+	processed_data[2] = processed_data[2] + [data[3][i] - data[0][i]] # difference of temple data
+	processed_data[3] = processed_data[3] + [data[2][i] - data[1][i]] # difference of brow data
+for i in range (0, 4):
+	processed_data[i] = (Filter(processed_data[i], order = 4, frequency = 10)[0])	
 
+####################################### plotting signal  ########################################### 
 
-# trace_data = [trace1]
-# fig = go.Figure(data=trace_data, layout=layout)
+t = list(range (0, len(processed_data[0])))
+fig, axs = plt.subplots(4, 2)
+for k in range (0, 4):
 
+	axs[k][0].plot(t, data[k])
+	axs[k][0].set_xlabel('time')
+	axs[k][0].set_ylabel('raw data')
+	axs[k][0].grid(True)
 
-plt.plot(x,y);
-plt.show()   
+	axs[k][1].plot(t, processed_data[k])
+	axs[k][1].set_xlabel('time')
+	axs[k][1].set_ylabel('processed data')
+	axs[k][1].grid(True)
 
-y_prime = FFT(y)
-x_prime = list(range (0, len(y_prime)))
-plt.plot(x_prime,y_prime)
-plt.show()   
+fig.tight_layout()
+plt.show()    
 
-y_prime = get_deri(x_prime,y_prime,256)
-plt.plot(x_prime,y_prime)
-plt.show()   
-
-# py.iplot(fig)
-
-# regularPlot(thing[3], 30)
-
-# for i in thing:
-# 	print(i)
-# k = - float(thing[0][0]) + float(thing[len(thing)-1][0])
-# counter = 0
-# for i in range (0, len(thing)):
-# 	for j in (2, 5):
-# 		try:
-# 			thing[i][j] = float(thing[i][j])
-# 			thing[i][j] = round(thing[i][j], 2)
-# 		except:
-# 			counter = counter+1
-# data = np.array(thing)
-# scrollingGraph(data[:,5])
+####################################### plotting signal end #############################################
