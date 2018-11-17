fec34c8883e92466e07b221a35ba17a48819598f
Shi Jie Wei <barneywei@hotmail.com>
2018-11-16 22:02:52 -0500

    Updated: - FIFO temp matrix for osc

--- SCRAP_CODES.py
+++ SCRAP_CODES.py
@@ -1,6 +1,2 @@
-import numpy as np
-
-temp_signal_3d_tensor = np.zeros((7, 7, 0))
-
-if __name__ == "__main__":
-    print(np.dstack((temp_signal_3d_tensor, np.ones((7, 7)))))
+array = [[1, 2, 3], [1, 2, 3]]
+print(len(array))
